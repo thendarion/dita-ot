c6a8f65da6f395ad64579dd83d0899a80f1a0908
thendarion <16006640+thendarion@users.noreply.github.com>
2025-06-23 15:43:25 +0200

    make things prettier
    Signed-off-by: thendarion <16006640+thendarion@users.noreply.github.com>

--- src/test/java/org/dita/dost/util/DITAOTCollatorTest.java
+++ src/test/java/org/dita/dost/util/DITAOTCollatorTest.java
@@ -10,7 +10,6 @@ package org.dita.dost.util;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 import java.util.Locale;
-
 import org.junit.jupiter.api.Test;
 
 public class DITAOTCollatorTest {
